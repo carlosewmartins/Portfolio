dadb40eb7650126ca5f247a5afd7b3889d2d2f6d
Carlos Eduardo Wille Martins <tekaxis.dev@gmail.com>
2025-08-19 10:40:06 -0300

    feat: introdução ao javascript na pagina, criação de efeito typewriter

---
+++ script.js
@@ -0,0 +1,6 @@
+const typed = new Typed("#typing", {
+    strings: ["Carlos Eduardo Wille Martins"], // apenas uma string
+    typeSpeed: 90, // velocidade de digitação
+    showCursor: true, // mostra o cursor
+    loop: true // não repete
+});
