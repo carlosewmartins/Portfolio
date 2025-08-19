75ff13ff9240ac59d54d2908d3b0a03c80fa01ec
Carlos Eduardo Wille Martins <tekaxis.dev@gmail.com>
2025-08-19 10:57:41 -0300

    feat: criação de animação typewriter funcional

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-const typed = new Typed("#typing", {
+/*const typed = new Typed("#typing", {
     strings: ["Carlos Eduardo Wille Martins"], // apenas uma string
     typeSpeed: 90, // velocidade de digitação
     showCursor: true, // mostra o cursor
     loop: true // não repete
 });
+*/
+function typeWriter (elemento) {
+    const textoArray = elemento.innerHTML.split('');
+    elemento.innerHTML = '';
+    textoArray.forEach((letra, i) => {
+        setTimeout(() => elemento.innerHTML += letra, 75 * i);
+    });
+}
+const titulo = document.querySelector("#typing")
+typeWriter(titulo)
